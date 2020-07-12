17550a3440d4dac0d5a43e10d0316fc6569d94ba
Peter Williamson <pete@willshawmedia.com>
2020-07-12 21:28:30 +0100

    cloudinary hosted nav icon

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,12 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <img
+            class="logo-small"
+            src="https://res.cloudinary.com/willshawmedia/image/upload/c_thumb,w_50,g_face/v1594585217/whereyouat/logo-small.png"
+        >
         <router-link class="navbar-brand" :to="{ path: '/' }">{{title}}</router-link>
 
-        </nav>
+    </nav>
 </template>
 
 <script>
@@ -10,3 +14,6 @@
         props: [ 'title' ]
     }
 </script>
+
+<style scoped>
+</style>
